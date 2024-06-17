1a5b1ae37f82dd2e48d4b34eb4a2bc5ca265542f
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-17 16:07:49 +0700

    Add OwnerProfile, UserProfile mapping profile

---
+++ DuLichNamDinh_BE/TND.Application/Mapping/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TND.Application.Users.Login;
+using TND.Application.Users.Register;
+using TND.Domain.Entities;
+using TND.Domain.Models;
+
+namespace TND.Application.Mapping
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<RegisterCommand, User>();
+            CreateMap<JwtToken, LoginResponse>();
+        }
+    }
+}
