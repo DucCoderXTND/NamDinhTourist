f06344db2e585092c7552ef03d4d2237f41ddff7
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-17 15:47:10 +0700

    Add login and register commands and handlers

---
+++ DuLichNamDinh_BE/TND.Application/Users/Login/LoginResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace TND.Application.Users.Login
+{
+    public record LoginResponse(string Token);
+}
---
+++ DuLichNamDinh_BE/TND.Application/Users/Register/RegisterCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using TND.Domain.Entities;
+using TND.Domain.Exceptions;
+using TND.Domain.Interfaces.Persistence;
+using TND.Domain.Interfaces.Persistence.Repositories;
+using TND.Domain.Messages;
+
+namespace TND.Application.Users.Register
+{
+    public class RegisterCommandHandler : IRequestHandler<RegisterCommand>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRoleRepository _roleRepository;
+
+        public RegisterCommandHandler(IUserRepository userRepository, IMapper mapper,
+            IUnitOfWork unitOfWork, IRoleRepository roleRepository)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+            _roleRepository = roleRepository;
+        }
+        public async Task Handle(RegisterCommand request, CancellationToken cancellationToken = default)
+        {
+            var role = await _roleRepository.GetByNameAsync(request.Role, cancellationToken)
+                ?? throw new InvalidRoleException(UserMessages.InvalidRole);
+
+            if(await _userRepository.ExistsByEmailAsync(request.Email, cancellationToken))
+            {
+                throw new UserWithEmailAlreadyExistsException(UserMessages.WithEmailExists);
+            }
+
+            var userToAdd = _mapper.Map<User>(request);
+            userToAdd.Roles.Add(role);
+
+            await _userRepository.CreateAsync(userToAdd, cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
