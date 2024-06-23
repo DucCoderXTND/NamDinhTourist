95c1f4b924c11266fbf62267b55446bd777fd63d
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-23 09:38:42 +0700

    Add add image to room class gallery command and handler

---
+++ DuLichNamDinh_BE/TND.Application/RoomClasses/AddImageToGallery/AddImageToRoomClassGalleryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace TND.Application.RoomClasses.AddImageToGallery
+{
+    public record AddImageToRoomClassGalleryCommand(
+        Guid RoomClassId,
+        IFormFile Image) : IRequest;
+}
---
+++ DuLichNamDinh_BE/TND.Application/RoomClasses/AddImageToGallery/AddImageToRoomClassGalleryCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using TND.Domain.Exceptions;
+using TND.Domain.Interfaces.Persistence;
+using TND.Domain.Interfaces.Persistence.Repositories;
+using TND.Domain.Messages;
+
+namespace TND.Application.RoomClasses.AddImageToGallery
+{
+    public class AddImageToRoomClassGalleryCommandHandler : IRequestHandler<AddImageToRoomClassGalleryCommand>
+    {
+        private readonly IRoomClassRepository _roomClassRepository;
+        private readonly IImageRepository _imageRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AddImageToRoomClassGalleryCommandHandler(IRoomClassRepository roomClassRepository,
+            IImageRepository imageRepository, IUnitOfWork unitOfWork)
+        {
+            _roomClassRepository = roomClassRepository;
+            _imageRepository = imageRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(AddImageToRoomClassGalleryCommand request, 
+            CancellationToken cancellationToken = default)
+        {
+            if(await _roomClassRepository.ExistsByIdAsync(request.RoomClassId))
+            {
+                throw new NotFoundException(RoomClassMessages.NotFound);
+            }
+
+            await _imageRepository.CreateAsync(
+                request.Image, request.RoomClassId,
+                Domain.Enums.ImageType.Gallery,
+                cancellationToken);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        }
+    }
+}
