64d8067495d7fc1f1af63bbafcc2d331c0c25699
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-23 09:27:39 +0700

    Add get room classes by hotel id for guest query and handler

---
+++ DuLichNamDinh_BE/TND.Application/RoomClasses/GetByHotelIdForGuest/GetRoomClassesByHotelIdForGuestQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TND.Domain.Models;
+
+namespace TND.Application.RoomClasses.GetByHotelIdForGuest
+{
+    public record GetRoomClassesByHotelIdForGuestQuery(
+        Guid HotelId,
+        int PageNumber,
+        int PageSize) : IRequest<PaginatedList<RoomClassForGuestResponse>>;
+}
---
+++ DuLichNamDinh_BE/TND.Application/RoomClasses/GetByHotelIdForGuest/RoomClassForGuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using TND.Application.RoomClasses.GetById;
+using TND.Domain.Enums;
+
+namespace TND.Application.RoomClasses.GetByHotelIdForGuest
+{
+    public record RoomClassForGuestResponse(
+        Guid Id,
+        string Name,
+        int AdultsCapacity,
+        int ChildrenCapacity,
+        decimal PricePerNight,
+        string? Description,
+        RoomType RoomType,
+        IEnumerable<Guid> Amenities,
+        DiscountResponse? ActiveDiscount,
+        IEnumerable<string> GalleryUrls);
+}
