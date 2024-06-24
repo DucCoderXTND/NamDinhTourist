b02ecf85a983241781308d50043caec41686421b
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-24 10:50:14 +0700

    Add DiscountProfile

---
+++ DuLichNamDinh_BE/TND.Application/Mapping/DiscountProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TND.Application.Discounts.Create;
+using TND.Application.RoomClasses.GetById;
+using TND.Domain.Entities;
+using TND.Domain.Models;
+
+namespace TND.Application.Mapping
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<PaginatedList<Discount>, PaginatedList<DiscountResponse>>()
+                .ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
+            CreateMap<CreateDiscountCommand, Discount>();
+            CreateMap<Discount, DiscountResponse>();
+        }
+    }
+}
