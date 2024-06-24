7419f757a8feccfa01c08b74ec093bfc098ab547
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-24 10:40:29 +0700

    Add ReviewProfile

---
+++ DuLichNamDinh_BE/TND.Application/Mapping/ReviewProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using TND.Application.Reviews.Common;
+using TND.Application.Reviews.Create;
+using TND.Application.Reviews.Update;
+using TND.Domain.Entities;
+using TND.Domain.Models;
+
+namespace TND.Application.Mapping
+{
+    public class ReviewProfile : Profile
+    {
+        public ReviewProfile()
+        {
+            CreateMap<Review, ReviewResponse>();
+            CreateMap<CreateReviewCommand, Review>();
+            CreateMap<UpdateReviewCommand, Review>();
+            CreateMap<PaginatedList<Review>, PaginatedList<ReviewResponse>>()
+                .ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
+
+        }
+    }
+}
