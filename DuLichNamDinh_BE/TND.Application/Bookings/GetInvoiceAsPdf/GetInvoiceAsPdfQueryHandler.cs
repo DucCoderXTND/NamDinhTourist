2793abd8c2cae0675dd7e9104517d221a2dfcd9b
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-25 08:33:09 +0700

    Add get booking invoice as pdf query and handler

---
+++ DuLichNamDinh_BE/TND.Application/Bookings/GetInvoiceAsPdf/GetInvoiceAsPdfQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using TND.Application.Bookings.Common;
+using TND.Domain.Exceptions;
+using TND.Domain.Interfaces.Persistence.Repositories;
+using TND.Domain.Interfaces.Services;
+using TND.Domain.Messages;
+
+namespace TND.Application.Bookings.GetInvoiceAsPdf
+{
+    public class GetInvoiceAsPdfQueryHandler : IRequestHandler<GetInvoiceAsPdfQuery, byte[]>
+    {
+        private readonly IBookingRepository _bookingRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IPdfService _pdfService;
+
+        public GetInvoiceAsPdfQueryHandler(IBookingRepository bookingRepository, IUserRepository userRepository,
+            IPdfService pdfService)
+        {
+            _bookingRepository = bookingRepository;
+            _userRepository = userRepository;
+            _pdfService = pdfService;
+        }
+        public async Task<byte[]> Handle(GetInvoiceAsPdfQuery request, CancellationToken cancellationToken = default)
+        {
+            if (!await _userRepository.ExistsByIdAsync(request.GuestId, cancellationToken))
+            {
+                throw new NotFoundException(UserMessages.NotFound);
+            }
+
+            var booking = await _bookingRepository
+                .GetByIdAsync(request.BookingId, request.GuestId, true, cancellationToken)
+                ?? throw new NotFoundException(BookingMessages.NotFoundForGuest);
+
+
+            return await _pdfService.GeneratePdfFromHtmlAsync(InvoiceDetailsGenerator.GetInvoiceHtml(booking), cancellationToken);
+        }
+    }
+}
