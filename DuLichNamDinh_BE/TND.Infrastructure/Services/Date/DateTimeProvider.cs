7a4f8f780952a1497aa2317735c820fb0e29a71f
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-25 16:26:39 +0700

    Add DateTimeProvider

---
+++ DuLichNamDinh_BE/TND.Infrastructure/Services/Date/DateTimeProvider.cs
@@ -0,0 +1,17 @@
+﻿using TND.Domain.Interfaces.Services;
+
+namespace TND.Infrastructure.Services.Date
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime GetCurrentDateTimeUtc()
+        {
+            return DateTime.UtcNow;
+        }
+
+        public DateOnly GetCurrentDateUtc()
+        {
+            return DateOnly.FromDateTime(DateTime.UtcNow);
+        }
+    }
+}
