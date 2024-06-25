9f07c57171f8e92dd7c28838c057cd32473931e9
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-25 10:33:38 +0700

    Add ApplicationConfiguration

---
+++ DuLichNamDinh_BE/TND.Application/ApplicationConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace TND.Application
+{
+    public static class ApplicationConfiguration
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
