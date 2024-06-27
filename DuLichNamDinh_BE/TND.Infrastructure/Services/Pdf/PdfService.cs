aca33fbf4183a068be9be21ba60476d4291e7d81
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-27 15:21:49 +0700

    Email and Pdf configuration

---
+++ DuLichNamDinh_BE/TND.Infrastructure/Services/Pdf/PdfService.cs
@@ -0,0 +1,17 @@
+﻿using NReco.PdfGenerator;
+using TND.Domain.Interfaces.Services;
+
+namespace TND.Infrastructure.Services.Pdf
+{
+    public class PdfService : IPdfService
+    {
+        public async Task<byte[]> GeneratePdfFromHtmlAsync(string html, CancellationToken cancellationToken = default)
+        {
+            return await Task.Run(() =>
+            {
+                var htmlToPdf = new HtmlToPdfConverter();
+                return htmlToPdf.GeneratePdf(html);
+            }, cancellationToken);
+        }
+    }
+}
