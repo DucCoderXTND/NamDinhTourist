4e0d2f285b8f42972ca2c6b39112291654ffa28e
DucCoderXT <tranhoangduc2023xt@gmail.com>
2024-06-29 15:48:47 +0700

    Seed Roles

--- DuLichNamDinh_BE/TND.Infrastructure/Persistence/Configurations/RoleConfiguration.cs
+++ DuLichNamDinh_BE/TND.Infrastructure/Persistence/Configurations/RoleConfiguration.cs
@@ -12,6 +12,19 @@ namespace TND.Infrastructure.Persistence.Configurations
 
             builder.HasMany(r => r.Users)
                 .WithMany(u => u.Roles);
+
+            builder.HasData([
+                new Role
+                {
+                    Id = new Guid("3d736b28-cf80-4dc1-8e49-453e3760f0be"),
+                    Name = "Guest"
+                },
+                new Role
+                {
+                    Id = new Guid("d2e7d2bb-bc77-4a6d-a43a-763716c6df8b"),
+                    Name = "Admin"
+                }
+            ]);
         }
     }
 }
